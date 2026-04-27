47a6f927eb23dcd276f7e1b393d6a737f6cef1e7
Alelopez4623 <alefamilylolob@outlook.es>
2026-04-27 17:43:58 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+function watajai (num: number) {
+    for (let index = 0; index < 4; index++) {
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            . . . . .
+            # . # . #
+            `)
+        basic.clearScreen()
+        music.play(music.tonePlayable(831, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
+        basic.showNumber(num)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("quiero")
+})
 function serial2 (text: string, num: number) {
     serial.writeValue(text, num)
     basic.showLeds(`
@@ -25,6 +43,9 @@ radio.onReceivedValue(function (name, value) {
         temp(value)
     } else if ("temp2" == name) {
         temp2(value)
+        if (47 < value) {
+            watajai(value)
+        }
     } else if ("pickup" == name) {
         basic.showIcon(IconNames.Yes)
         basic.clearScreen()
